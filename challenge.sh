0c5653fe8f5ba4cd1a54447468066c4b528fd221
Sergio Moreno <sergio.moreno@scotiabankcolpatria.com>
2022-07-28 20:22:37 -0500

    first commit

---
+++ challenge.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+a="hudson"
+b="2005"
+c="java"
+
+if [[ $1 = $a && $2 = $b && $3 = $c ]]; then
+   touch greetings.txt
+   echo "*************************************************************" >> greetings.txt
+   echo "* + Well done pal. U have earned some applauses points!!! + *" >> greetings.txt
+   echo "*************************************************************" >> greetings.txt
+   exit 0
+else
+    echo "BAD"
+    exit 1
+fi
