1018318f87b3d593e9af777cd9245bdada504ed5
Sergio Moreno <sergio.moreno@scotiabankcolpatria.com>
2022-07-28 20:44:27 -0500

    pp

--- challenge.sh
+++ challenge.sh
@@ -3,7 +3,7 @@ a="hudson"
 b="2005"
 c="java"
 
-if [$1 = $a && $2 = $b && $3 = $c ]; then
+if [[ "$1" = "$a" && "$2" = "$b" && "$3" = "$c" ]]; then
    touch greetings.txt
    echo "*************************************************************" >> greetings.txt
    echo "* + Well done pal. U have earned some applauses points!!! + *" >> greetings.txt
